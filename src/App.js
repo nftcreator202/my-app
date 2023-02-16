0ca60ef93116fd49cb27184a1dd4a869c9012bc7
nftcreator202 <nft.creator202@gmail.com>
2023-02-16 16:18:34 +0100

    no mint

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Home from "./Routes/Home/Home.component";
 import Project from "./Routes/Project/Project.component";
 import Artist from "./Routes/Artist/Artist.component";
-import Mint from "./Routes/Mint/Mint.component";
 
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
@@ -14,7 +13,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/project" element={<Project />} />
         <Route exact path="/artist" element={<Artist />} />
-        <Route exact path="/mint" element={<Mint />} />
+
       </Routes>
     </BrowserRouter>
   );
