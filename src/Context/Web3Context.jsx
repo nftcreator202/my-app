73b7235f49fb0753abdf821a0441b3f5646f6f2a
nftcreator202 <nft.creator202@gmail.com>
2023-04-07 15:58:26 +0200

    test

--- src/Context/Web3Context.jsx
+++ src/Context/Web3Context.jsx
@@ -26,7 +26,7 @@ export const Web3Provider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState([]);
   const [mintAmount, setMintAmount] = useState(1);
   const isConnected = Boolean(currentAccount[0]);
-  const [saleState, setSaleState] = useState(0);
+  const [saleState, setSaleState] = useState(1);
   const [proof, setProof] = useState(false);
 
   useEffect(() => {
